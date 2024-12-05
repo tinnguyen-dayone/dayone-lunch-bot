60773586384782153d8b8853d120dcb2ffa348c8
tinnguyen-dayone <tinnguyen@dayoneteams.com>
2024-12-05 16:55:29 +0700

    chore: update actions

--- config/settings.py
+++ config/settings.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-DB_URL = os.getenv('DB_URL')
+DB_URL = os.getenv('DB_URL') 
 LUNCH_PRICE = os.getenv('LUNCH_PRICE', '55.000 VND')
